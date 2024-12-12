92ffb66d68e9a2a33f7409ee9f3aeb99177e40cd
richardwds <richard@whydigit.in>
2024-12-12 15:25:20 +0530

    thirsdpartyinspection api done

--- src/main/java/com/efitops/basesetup/controller/GrnController.java
+++ src/main/java/com/efitops/basesetup/controller/GrnController.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.efitops.basesetup.common.CommonConstant;
 import com.efitops.basesetup.common.UserConstants;
@@ -24,14 +26,15 @@ import com.efitops.basesetup.dto.GrnDTO;
 import com.efitops.basesetup.dto.ResponseDTO;
 import com.efitops.basesetup.dto.ThirdPartyInspectionDTO;
 import com.efitops.basesetup.entity.GrnVO;
+import com.efitops.basesetup.entity.RouteCardEntryVO;
 import com.efitops.basesetup.entity.ThirdPartyInspectionVO;
 import com.efitops.basesetup.service.GrnService;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/grn")
-public class GrnController extends BaseController{
-	
+public class GrnController extends BaseController {
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(GrnController.class);
 
 	@Autowired
@@ -63,7 +66,7 @@ public class GrnController extends BaseController{
 		return ResponseEntity.ok().body(responseDTO);
 
 	}
-	
+
 	@GetMapping("/getGrnById")
 	public ResponseEntity<ResponseDTO> getItemById(@RequestParam Long id) {
 		String methodName = "getGrnById()";
@@ -90,7 +93,7 @@ public class GrnController extends BaseController{
 		return ResponseEntity.ok().body(responseDTO);
 
 	}
-	
+
 	@PutMapping("/updateCreateGrn")
 	public ResponseEntity<ResponseDTO> updateCreateGrn(@RequestBody GrnDTO grnDTO) {
 		String methodName = "updateCreateGrn()";
@@ -111,7 +114,7 @@ public class GrnController extends BaseController{
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 	}
-	
+
 	@GetMapping("/getGrnDocId")
 	public ResponseEntity<ResponseDTO> getGrnDocId(@RequestParam Long orgId) {
 
@@ -134,14 +137,14 @@ public class GrnController extends BaseController{
 			responseObjectsMap.put("grnDocId", mapp);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
-			responseDTO = createServiceResponseError(responseObjectsMap,
-					"Failed to retrieve GrnDocId information", errorMsg);
+			responseDTO = createServiceResponseError(responseObjectsMap, "Failed to retrieve GrnDocId information",
+					errorMsg);
 		}
 
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 	}
-	
+
 	@GetMapping("/getInwardNoForGRN")
 	public ResponseEntity<ResponseDTO> getInwardNoForGRN(@RequestParam(required = false) Long orgId) {
 		String methodName = "getInwardNoForGRN()";
@@ -162,17 +165,103 @@ public class GrnController extends BaseController{
 			responseObjectsMap.put("inwardforgrn", inwardforgrn);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
-			responseDTO = createServiceResponseError(responseObjectsMap, "Inward for Grn information receive failed By OrgId",
-					errorMsg);
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"Inward for Grn information receive failed By OrgId", errorMsg);
 		}
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 
 	}
-	
+
+	@GetMapping("/getSupplierAddressForGRN")
+	public ResponseEntity<ResponseDTO> getSupplierAddressForGRN(@RequestParam(required = false) Long orgId,
+			String supplierName) {
+		String methodName = "getSupplierAddressForGRN()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		List<Map<String, Object>> supplieraddressforgrn = new ArrayList<>();
+		try {
+			supplieraddressforgrn = grnService.getSupplierAddressForGRN(orgId, supplierName);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					" supplieraddress for Grn information get successfully By OrgId");
+			responseObjectsMap.put("supplieraddressforgrn", supplieraddressforgrn);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"supplieraddress For Grn information receive failed By OrgId", errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+
+	}
+
+	@GetMapping("/getSGSTandCGSTForGRN")
+	public ResponseEntity<ResponseDTO> getSGSTandCGSTForGRN(@RequestParam(required = false) Long orgId, String taxType,
+			String gstType) {
+		String methodName = "getSGSTandCGSTForGRN()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		List<Map<String, Object>> cgstandsgst = new ArrayList<>();
+		try {
+			cgstandsgst = grnService.getSGSTandCGSTForGRN(orgId, taxType, gstType);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					" CGST and SGST for Grn information get successfully By OrgId");
+			responseObjectsMap.put("cgstandsgst", cgstandsgst);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"CGST and SGST for Grn information receive failed By OrgId", errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+
+	}
+
+	@GetMapping("/getIGSTForGRN")
+	public ResponseEntity<ResponseDTO> getIGSTForGRN(@RequestParam(required = false) Long orgId, String taxType,
+			String gstType) {
+		String methodName = "getIGSTForGRN()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		List<Map<String, Object>> igst = new ArrayList<>();
+		try {
+			igst = grnService.getIGSTForGRN(orgId, taxType, gstType);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					" IGST for Grn information get successfully By OrgId");
+			responseObjectsMap.put("igst", igst);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"IGST for Grn information receive failed By OrgId", errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+
+	}
+
 	@GetMapping("/getItemForGRN")
-	public ResponseEntity<ResponseDTO> getItemForGRN(@RequestParam(required = false) Long orgId,
-			String inwardNo) {
+	public ResponseEntity<ResponseDTO> getItemForGRN(@RequestParam(required = false) Long orgId, String inwardNo) {
 		String methodName = "getItemForGRN()";
 		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
 		String errorMsg = null;
@@ -180,7 +269,7 @@ public class GrnController extends BaseController{
 		ResponseDTO responseDTO = null;
 		List<Map<String, Object>> itemforgrn = new ArrayList<>();
 		try {
-			itemforgrn = grnService.getItemForGRN(orgId,inwardNo);
+			itemforgrn = grnService.getItemForGRN(orgId, inwardNo);
 		} catch (Exception e) {
 			errorMsg = e.getMessage();
 			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
@@ -191,14 +280,14 @@ public class GrnController extends BaseController{
 			responseObjectsMap.put("itemforgrn", itemforgrn);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
-			responseDTO = createServiceResponseError(responseObjectsMap, "itemforgrn for Grn information receive failed By OrgId",
-					errorMsg);
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"itemforgrn for Grn information receive failed By OrgId", errorMsg);
 		}
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 
 	}
-	
+
 	@GetMapping("/getThirdPartyInspectionByOrgId")
 	public ResponseEntity<ResponseDTO> getThirdPartyInspectionByOrgId(@RequestParam Long orgId) {
 		String methodName = "getThirdPartyInspectionByOrgId()";
@@ -218,8 +307,64 @@ public class GrnController extends BaseController{
 			responseObjectsMap.put("thirdPartyInspectionVO", thirdPartyInspectionVO);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
-			responseDTO = createServiceResponseError(responseObjectsMap, "Third Party information information receive failedByOrgId",
-					errorMsg);
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"Third Party information information receive failedByOrgId", errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+
+	}
+
+	@GetMapping("/getGRNForThirdPartyInsp")
+	public ResponseEntity<ResponseDTO> getGRNForThirdPartyInsp(@RequestParam(required = false) Long orgId) {
+		String methodName = "getGRNForThirdPartyInsp()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		List<Map<String, Object>> chCode = new ArrayList<>();
+		try {
+			chCode = grnService.getGRNForThirdPartyInsp(orgId);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					" GRNForThirdPartyInsp for Grn information get successfully By OrgId");
+			responseObjectsMap.put("chCode", chCode);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"GRNForThirdPartyInsp for Grn information receive failed By OrgId", errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+
+	}
+
+	@GetMapping("/getThirdPartyDetailsForThirdPartyInsp")
+	public ResponseEntity<ResponseDTO> getThirdPartyDetailsForThirdPartyInsp(@RequestParam(required = false) Long orgId) {
+		String methodName = "getThirdPartyDetailsForThirdPartyInsp()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		List<Map<String, Object>> chCode = new ArrayList<>();
+		try {
+			chCode = grnService.getThirdPartyDetailsForThirdPartyInsp(orgId);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					" ThirdPartyDetails for Grn information get successfully By OrgId");
+			responseObjectsMap.put("chCode", chCode);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"ThirdPartyDetails for Grn information receive failed By OrgId", errorMsg);
 		}
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
@@ -241,20 +386,22 @@ public class GrnController extends BaseController{
 			LOGGER.error(UserConstants.ERROR_MSG_METHOD_NAME, methodName, errorMsg);
 		}
 		if (StringUtils.isBlank(errorMsg)) {
-			responseObjectsMap.put(CommonConstant.STRING_MESSAGE, "ThirdPartyInspection information get successfully By Id");
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					"ThirdPartyInspection information get successfully By Id");
 			responseObjectsMap.put("thirdPartyInspectionVO", thirdPartyInspectionVO);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
-			responseDTO = createServiceResponseError(responseObjectsMap, "ThirdPartyInspection information receive failed By Id",
-					errorMsg);
+			responseDTO = createServiceResponseError(responseObjectsMap,
+					"ThirdPartyInspection information receive failed By Id", errorMsg);
 		}
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 
 	}
-	
+
 	@PutMapping("/updateCreateThirdPartyInsp")
-	public ResponseEntity<ResponseDTO> updateCreateThirdPartyInsp(@RequestBody ThirdPartyInspectionDTO thirdPartyInspectionDTO) {
+	public ResponseEntity<ResponseDTO> updateCreateThirdPartyInsp(
+			@RequestBody ThirdPartyInspectionDTO thirdPartyInspectionDTO) {
 		String methodName = "updateCreateThirdPartyInsp()";
 		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
 		String errorMsg = null;
@@ -273,9 +420,9 @@ public class GrnController extends BaseController{
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 	}
-	
+
 	@GetMapping("/getThirdPartyInspectionDocId")
-	public ResponseEntity<ResponseDTO> getThirdPartyInspectionDocId (@RequestParam Long orgId) {
+	public ResponseEntity<ResponseDTO> getThirdPartyInspectionDocId(@RequestParam Long orgId) {
 
 		String methodName = "getThirdPartyInspectionDocId()";
 		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
@@ -292,7 +439,8 @@ public class GrnController extends BaseController{
 		}
 
 		if (StringUtils.isBlank(errorMsg)) {
-			responseObjectsMap.put(CommonConstant.STRING_MESSAGE, "ThirdPartyInspectionDocId information retrieved successfully");
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE,
+					"ThirdPartyInspectionDocId information retrieved successfully");
 			responseObjectsMap.put("thirdPartyInspectionDocId", mapp);
 			responseDTO = createServiceResponse(responseObjectsMap);
 		} else {
@@ -303,6 +451,32 @@ public class GrnController extends BaseController{
 		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
 		return ResponseEntity.ok().body(responseDTO);
 	}
-	
-		
+
+	@PostMapping("/uploadFileForThirdPartyInspection")
+	public ResponseEntity<ResponseDTO> uploadFileForThirdPartyInspection(@RequestParam("file") MultipartFile file,
+			@RequestParam Long id) {
+		String methodName = "uploadFileForThirdPartyInspection()";
+		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
+		String errorMsg = null;
+		Map<String, Object> responseObjectsMap = new HashMap<>();
+		ResponseDTO responseDTO = null;
+		ThirdPartyInspectionVO thirdPartyInspectionVO = null;
+		try {
+			thirdPartyInspectionVO = grnService.uploadFileForThirdPartyInspection(file, id);
+		} catch (Exception e) {
+			errorMsg = e.getMessage();
+			LOGGER.error("Unable To Upload File", methodName, errorMsg);
+		}
+		if (StringUtils.isBlank(errorMsg)) {
+			responseObjectsMap.put(CommonConstant.STRING_MESSAGE, "FileUpload for RouteCardEntry Successfully Upload");
+			responseObjectsMap.put("thirdPartyInspectionVO", thirdPartyInspectionVO);
+			responseDTO = createServiceResponse(responseObjectsMap);
+		} else {
+			responseDTO = createServiceResponseError(responseObjectsMap, "FileUpload for RouteCardEntry Upload Failed",
+					errorMsg);
+		}
+		LOGGER.debug(CommonConstant.ENDING_METHOD, methodName);
+		return ResponseEntity.ok().body(responseDTO);
+	}
+
 }
--- src/main/java/com/efitops/basesetup/service/GrnService.java
+++ src/main/java/com/efitops/basesetup/service/GrnService.java
@@ -1,13 +1,16 @@
 package com.efitops.basesetup.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.efitops.basesetup.dto.GrnDTO;
 import com.efitops.basesetup.dto.ThirdPartyInspectionDTO;
 import com.efitops.basesetup.entity.GrnVO;
+import com.efitops.basesetup.entity.RouteCardEntryVO;
 import com.efitops.basesetup.entity.ThirdPartyInspectionVO;
 import com.efitops.basesetup.exception.ApplicationException;
 
@@ -27,16 +30,31 @@ public interface GrnService {
 	
 	List<Map<String, Object>> getItemForGRN(Long orgId,String InwardNo);
 
+	List<Map<String, Object>> getSupplierAddressForGRN(Long orgId, String supplierName);
+	
+	List<Map<String, Object>> getSGSTandCGSTForGRN(Long orgId,String taxType,String gstType );
+	
+	List<Map<String, Object>> getIGSTForGRN(Long orgId,String taxType,String gstType );
+	
+	//third party inspection
 	List<ThirdPartyInspectionVO> getThirdPartyInspByOrgId(Long orgId);
 
 	List<ThirdPartyInspectionVO> getThirdPartyInspById(Long id);
 	
+	List<Map<String,Object>> getGRNForThirdPartyInsp(Long orgId);
+	
+	List<Map<String,Object>> getThirdPartyDetailsForThirdPartyInsp(Long orgId);
+	
+
+	
+	
 	Map<String, Object> updateCreateThirdPartyInsp (ThirdPartyInspectionDTO thirdPartyInspectionDTO) throws ApplicationException;
 	
-	List<Map<String, Object>> getGRNDetForThirdPartyInsp (Long orgId);
-
 
 	String getThirdPartyInspectionDocId(Long orgId);
+	
+	ThirdPartyInspectionVO uploadFileForThirdPartyInspection(MultipartFile file, Long id) throws IOException;
+
 
 
 	//Map<String, Object> updateCreateThirdPartyInspection(ThirdPartyInspectionDTO thirdPartyInspectionDTO);
